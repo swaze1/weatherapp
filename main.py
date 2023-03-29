4c0790a6e9aec91cebd32798e88609ddef2adec0
swaze1 <axzel515@gmail.com>
2023-03-29 17:06:54 +0530

    Exception handler

--- main.py
+++ main.py
@@ -8,37 +8,41 @@ app = Flask(__name__)
 @app.route("/", methods = ['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        city_name = request.form['city']
+        try:
+            city_name = request.form['city']
 
-        url = f"http://api.weatherapi.com/v1/current.json?key={config.KEY}&q={city_name}"
-        response = requests.get(url.format(city_name)).json()
+            url = f"http://api.weatherapi.com/v1/current.json?key={config.KEY}&q={city_name}"
+            response = requests.get(url.format(city_name)).json()
 
-        name = response['location']['name']
-        region = response['location']['region']
-        temp_c = response['current']['temp_c']
-        temp_f = response['current']['temp_f']
-        wind_speed = response['current']['wind_kph']
-        wind_direction = response['current']['wind_dir']
-        humidity = response['current']['humidity']
-        clouds = response['current']['cloud']
-        text = response['current']['condition']['text']
-        icon = response['current']['condition']['icon']
-        vis_km = response['current']['vis_km']
-        time = datetime.now()
+            name = response['location']['name']
+            region = response['location']['region']
+            temp_c = response['current']['temp_c']
+            temp_f = response['current']['temp_f']
+            wind_speed = response['current']['wind_kph']
+            wind_direction = response['current']['wind_dir']
+            humidity = response['current']['humidity']
+            clouds = response['current']['cloud']
+            text = response['current']['condition']['text']
+            icon = response['current']['condition']['icon']
+            vis_km = response['current']['vis_km']
+            time = datetime.now()
 
-        return render_template("city.html", name=name,
-        region=region,
-        temp_c=temp_c,
-        temp_f=temp_f,
-        wind_direction=wind_direction,
-        humidity=humidity,
-        clouds=clouds,
-        icon=icon,
-        time=time,
-        wind_speed=wind_speed,
-        vis_km=vis_km,
-        text=text)
+            return render_template("city.html", name=name,
+            region=region,
+            temp_c=temp_c,
+            temp_f=temp_f,
+            wind_direction=wind_direction,
+            humidity=humidity,
+            clouds=clouds,
+            icon=icon,
+            time=time,
+            wind_speed=wind_speed,
+            vis_km=vis_km,
+            text=text)
 
+        except KeyError as k:
+
+            return render_template("index.html")
     else:
 
         return render_template("index.html")
